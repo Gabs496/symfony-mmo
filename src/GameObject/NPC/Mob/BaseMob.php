e7225008d7c31127ac6e71315f30c9a39150baaa
Antonio Valerio <antonio.valerio@performer.it>
2025-03-14 18:19:29 +0100

    Introducing mob concept

--- src/GameObject/NPC/Mob/BaseMob.php
+++ src/GameObject/NPC/Mob/BaseMob.php
@@ -4,18 +4,19 @@ namespace App\GameObject\NPC\Mob;
 
 use App\Engine\Combat\RewardOnDefeatInterface;
 
-abstract class BaseMob extends \App\GameElement\NPC\BaseMob implements RewardOnDefeatInterface
+abstract readonly class BaseMob extends \App\GameElement\NPC\BaseMob implements RewardOnDefeatInterface
 {
     protected string $icon;
 
     public function __construct(
+        string $id,
         string $name,
         float $maxHealth,
         string $description,
         ?string $icon = null
     )
     {
-        parent::__construct($name, $maxHealth, $description);
+        parent::__construct($id, $name, $maxHealth, $description);
         $this->icon = $icon ?? '/mob/' . strtolower(get_class($this)) . '.png';
     }
 }
