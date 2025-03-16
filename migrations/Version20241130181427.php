eed140860ae024ba79a3e57ca13fdf6d9ff2583a
Antonio Valerio <antonio.valerio@performer.it>
2025-03-16 03:07:54 +0100

    Migrations fix

--- migrations/Version20241130181427.php
+++ migrations/Version20241130181427.php
@@ -21,7 +21,7 @@ final class Version20241130181427 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE security_user (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE data_player_character ADD user_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\'');
+        $this->addSql('ALTER TABLE data_player_character ADD position_id VARCHAR(50) NOT NULL, ADD user_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\'');
         $this->addSql('ALTER TABLE data_player_character ADD CONSTRAINT FK_B0D0B123A76ED395 FOREIGN KEY (user_id) REFERENCES security_user (id)');
         $this->addSql('ALTER TABLE data_player_character ADD CONSTRAINT FK_B0D0B123DD842E46 FOREIGN KEY (position_id) REFERENCES game_map (id)');
         $this->addSql('CREATE INDEX IDX_B0D0B123A76ED395 ON data_player_character (user_id)');
